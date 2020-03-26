d3a09582fbc43038cc8fe7b3214b4abf4092bace
alexis <adelaforge@lirmm.fr>
2020-03-26 20:55:55 +0100

    test useless

--- train.py
+++ train.py
@@ -46,3 +46,15 @@ def full_train(parameters):
                     total_loss = 0
                     start_time = time.time()
             else :
+
+def evaluate(eval_model, data_source):
+    eval_model.eval()  # Turn on the evaluation mode
+    total_loss = 0.
+    ntokens = len(TEXT.vocab.stoi)
+    with torch.no_grad():
+        for i in range(0, data_source.size(0) - 1, bptt):
+            data, targets = get_batch(data_source, i)
+            output = eval_model(data)
+            output_flat = output.view(-1, ntokens)
+            total_loss += len(data) * criterion(output_flat, targets).item()
+    return total_loss / (len(data_source) - 1)
